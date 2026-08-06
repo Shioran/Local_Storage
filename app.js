9945105178ff11ed3e691abb453c7d304ed0398a
Shioran <andresmars45@gmail.com>
2026-08-06 16:27:17 -0500

    se crea la funcion getproducto para ver los productos en el documento js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let precio = document.getElementById("precioPro");
 let cantidad = document.getElementById("cantidadPro");
 let imagen = document.getElementById("imagenPro");
 let btnGuardar = document.querySelector(".btnGuardar")
+let tabla = document.querySelector(".table > tbody")
 
 //agregarle un evento al boton
 btnGuardar.addEventListener("click", function () {
@@ -10,8 +11,11 @@ btnGuardar.addEventListener("click", function () {
 
     if (pro) {
         saveProduct(pro);
+        tabla.innerHTML = "";   // limpiar tbody
+        getProducts();
         console.log("producto", pro);
     }
+
 });
 
 //obetner y validar
@@ -44,3 +48,30 @@ function saveProduct(product){
     localStorage.setItem("lista-pro", JSON.stringify(productPrevius));
     alert("producto guardado con exito 👍")
 }
+
+function getProducts(){
+    console.log("Entré a getProducts");
+    let productPrevius = JSON.parse(localStorage.getItem("lista-pro")) || [];
+    if (productPrevius != null) {
+        productos = productPrevius;
+    }
+
+    productPrevius.forEach((pro , i) => {
+        let fila = document.createElement("tr");
+        fila.innerHTML = `
+        <td>${i + 1}</td>
+        <td>${pro.nombre}</td>
+        <td>${pro.precio}</td>
+        <td>${pro.cantidad}</td>
+        <td> <img src="${pro.imagen}" width="100px" height="100px"></td>
+        <td>
+            <span class="btn btn-danger" onclick="deleteProduct(${i})">Eliminar</span>
+            <span class="btn btn-warning" onclick="editProduct(${i})">Editar</span>
+        </td>
+        `;
+        tabla.appendChild(fila);
+});
+}
+document.addEventListener("DOMContentLoaded", function () {
+    getProducts();
+})
